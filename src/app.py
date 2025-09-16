3567424170b9b3a4b9b8a8eaaa2d611973993dc8
Aditya <dimpalrai520@gmail.com>
2025-09-16 10:03:06 +0530

    Clean up app.py header to fix syntax error

--- src/app.py
+++ src/app.py
@@ -1,11 +1,15 @@
 from keep_alive import keep_alive
 keep_alive()
 
-import os, asyncio, discord
+import os
+import asyncio
+import discord
 from discord.ext import commands
-from src/core.db import init_db
-from src/core.logger import log
-from src/services.prefixes import get_prefix
+
+from src.core.db import init_db
+from src.core.logger import log
+from src.services.prefixes import get_prefix
+
 
 TOKEN = os.environ.get("DISCORD_TOKEN")
 
